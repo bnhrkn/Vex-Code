36e41a7286934aa12d1671f341394238ea799714
Ben Harkin <bnhrkn@protonmail.com>
2022-08-08 12:31:25 -0400

    Add 3 drive modes and drive mode selection

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include "main.h"
+#include "okapi/api/chassis/model/skidSteerModel.hpp"
+#include "okapi/api/device/motor/abstractMotor.hpp"
+#include "okapi/impl/device/button/controllerButton.hpp"
+#include "okapi/impl/device/controllerUtil.hpp"
+#include "pros/misc.h"
+#include "pros/misc.hpp"
 #include "pros/rtos.hpp"
+#include <atomic>
+#include <cstddef>
+#include <iterator>
 #include <memory>
+#include <string>
+#include <string_view>
+#include <vector>
 
 using namespace okapi::literals;
 
@@ -23,6 +35,7 @@ void autonomous() {}
 
 void opcontrol() {
   okapi::Controller controller;
+
   auto model{drive->getModel()};
 
   pros::Task matchTimer{[&] {
@@ -35,11 +48,60 @@ void opcontrol() {
     controller.rumble("-"); // Rumble on endgame start at 1:35
   }};
 
+  constexpr std::array<std::string_view, 3> driveModes = {"arcade", "curvature",
+                                                          "tank"};
+  std::atomic<std::size_t> driveMode = 0;
+
+  pros::Task modeSwitcher{
+      [&] { // Task which allows the driver to select different modes nicely
+        auto i = driveMode.load();
+        okapi::ControllerButton yBtn{okapi::ControllerDigital::Y};
+
+        controller.setText( // Set the initial drive mode text
+            0, 0,
+            std::string("mode: ").append(driveModes.at(driveMode.load())));
+
+        while (true) {
+          if (yBtn.isPressed()) {
+            driveMode = i;
+            controller.setText(0, 0,
+                               std::string("mode: ")
+                                   .append(driveModes.at(driveMode))
+                                   .append(std::string_view("          ")));
+            if (i + 1 < driveModes.size()) {
+              i++;
+            } else {
+              i = 0;
+            }
+            int delay = 600;
+            for (int i = 0; i < delay && !yBtn.isPressed(); i += 50) {
+              pros::delay(50);
+            }
+          } else {
+            pros::delay(50);
+          }
+        }
+      }};
+
+  model->setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
+
   while (true) {
-    model->driveVector(controller.getAnalog(okapi::ControllerAnalog::leftY),
-                  controller.getAnalog(okapi::ControllerAnalog::leftX));
+
+    switch (driveMode.load()) {
+    case 1:
+      model->curvature(controller.getAnalog(okapi::ControllerAnalog::leftY),
+                       controller.getAnalog(okapi::ControllerAnalog::rightX));
+      break;
+    case 2:
+      model->tank(controller.getAnalog(okapi::ControllerAnalog::leftY),
+                  controller.getAnalog(okapi::ControllerAnalog::rightY));
+      break;
+    default:
+      model->arcade(controller.getAnalog(okapi::ControllerAnalog::leftY),
+                    controller.getAnalog(okapi::ControllerAnalog::rightX));
+      break;
+    }
 
     pros::delay(10);
   }
-  
 }
