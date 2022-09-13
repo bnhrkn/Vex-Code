b6b0d800f0e1619267ad44d278c434a16d1df7bd
Ben Harkin <bnhrkn@protonmail.com>
2022-09-13 10:46:03 -0400

    New RamseteController class

---
+++ include/project/ramsete.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "geometry/pose.hpp"
+#include "geometry/profilepoint.hpp"
+#include "main.h"
+
+class RamseteController {
+public:
+  RamseteController(const double ib, const double izeta);
+  std::pair<double, double> step(const squiggles::Pose&, const squiggles::ProfilePoint&);
+
+protected:
+  double gain, zeta, b;
+};
